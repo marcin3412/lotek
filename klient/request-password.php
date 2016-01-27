15d1268e9f04e71a0efa6d266a8531f9d32d60f6
Marcin Wojciechowski <m.wojciechowski20@gmail.com>
2016-01-27 19:51:06 +0100

    request password - dodano formularz

--- klient/request-password.php
+++ klient/request-password.php
@@ -70,5 +70,28 @@
 			KONIEC SEKCJI TOPMENU
 		------------>
 
+
+<form id="rev-pass" class="container">
+  <div class="form-group">
+    <label class="col-sm-2 control-label ">Email</label>
+    <div class="col-sm-10">
+      <input type="email" class="form-control" name="email" placeholder="Email">
+    </div>
+  </div>
+  <div class="form-group">
+    <label  class="col-sm-2 control-label">Hasło</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control" name="haslo" placeholder="Hasło">
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-sm-offset-2 col-sm-10">
+      <button type="submit" class="btn btn-default" name="resetuj">Resetuj Hasło</button>
+    </div>
+  </div>
+</form> 
+</div>
+
+
     </body>
 </html>
