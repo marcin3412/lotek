6b539de0b1166647ceace6bb90c0e16bd46b2e25
rekah4 <rekah4@gmail.com>
2016-01-27 21:08:16 +0100

    Dodano skrypt rejestracji

--- klient/js/rejestruj.js
+++ klient/js/rejestruj.js
@@ -1,26 +1,33 @@
 $(document).ready(function(){
-var form = $('#rejestracja').closest('form');
-
-
-
+if(!no_server_mode){
+	////////////////////////////////////////////////////////////////////
+	var form = $('#rejestracja').closest('form');
 	form.find('input[name=rejestracja]').click(function(event) {
-	
+	console.log(form.find('input').serializeArray());
 		$.ajax({
-
-			method: 'GET',
-			url: 'http://192.168.0.10:8080/trunk/account/register?',
+			url: 'http://127.0.0.1:8080/trunk/account/register',
+			dataType: "jsonp",
+			jsonpCallback: 'jsonpCallback',	
 			data: form.find('input').serializeArray(),
 			success: function(response){
-				
 					console.log(response);
-				
-			
-
-			},
-			error:function(response){
-
 			}
 		});
 		
 	});
+}
+else{
+	///////////////////////////////////
+	var form = $('#rejestracja').closest('form');
+	form.find('input[name=rejestracja]').click(function(event) {
+		data = JSON.parse('{"msg":"Dodano uzytkownika do bazy","status":"OK"}');
+		//data = JSON.parse('{"msg":"Brak parametrów w zapytaniu.","status":"ERROR"}'');
+		//data = JSON.parse('{"msg":"Użytkownik już istnieje.","status":"ERROR"}'');
+
+		//no i tutaj piszesz co ma sie kiedy dziać itd;
+
+		//data.msg  <-- tak odczytujez zmienne
+	});	
+}
+	
 });
